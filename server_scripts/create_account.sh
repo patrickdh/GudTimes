26734ca24ff768e81c5762091b5203b3c008ed2b
Ryan Baxter <12rab7@queensu.ca>
2015-11-01 18:15:38 -0500

    Adding username validation to account creation script.

--- server_scripts/create_account.sh
+++ server_scripts/create_account.sh
@@ -6,12 +6,19 @@ then
 	exit 1
 fi
 
+usercharsre='^[0-9a-zA-Z_]+$'
+
+if ! [[ $1 =~ $usercharsre ]] ; then
+	echo "Invalid username"
+	exit 3
+fi
+
 if id -u $1 > /dev/null 2>&1; then
 	echo "User already exists"
 	exit 2
 fi
 
-useradd -M -G sshuser -U $1
+useradd -M -G sftponly -U $1
 echo "$1:$2" | chpasswd
 mkdir /gudtimes/userfiles/$1
 chown $1 /gudtimes/userfiles/$1
