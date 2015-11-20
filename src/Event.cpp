66cc94c98fb139dd73493a177b5c88c0190c2c69
Ryan Baxter <12rab7@queensu.ca>
2015-11-20 18:02:12 -0500

    Event class implementation

--- src/Event.cpp
+++ src/Event.cpp
@@ -4,45 +4,63 @@ using namespace std;
 
 Event::Event()
 {
-    //ctor
+    valid = false;
 }
 
 Event::Event(const std::string& eventTitle, const wxDateTime& start, const wxDateTime& finish)
+: title(eventTitle), startDT(start), endDT(finish)
 {
-
+    freq = FrequencyEnum::NONE;
+    repeatCount = -1;
+    if (!startDT.IsLaterThan(endDT))
+    {
+        valid = true;
+    }
+    else
+    {
+        valid = false;
+    }
 }
 
 Event::Event(const std::string& eventTitle, const wxDateTime& start, const wxDateTime& finish, FrequencyEnum frequency, int repeatNum)
+: title(eventTitle), startDT(start), endDT(finish), freq(frequency), repeatCount(repeatNum)
 {
-
+    if (!startDT.IsLaterThan(endDT))
+    {
+        valid = true;
+    }
+    else
+    {
+        valid = false;
+    }
 }
 
 std::string Event::getTitle()
 {
-
+    return title;
 }
 
 wxDateTime Event::getStart()
 {
-
+    return startDT;
 }
 
 wxDateTime Event::getEnd()
 {
-
+    return endDT;
 }
 
 FrequencyEnum Event::getFrequency()
 {
-
+    return freq;
 }
 
 int Event::getRepeatCount()
 {
-
+    return repeatCount;
 }
 
 bool Event::isValid()
 {
-
+    return valid;
 }
