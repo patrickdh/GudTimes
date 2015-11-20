e2f2113cd3e74b61f0c804c45fa1621c70cf3cf2
Ryan Baxter <12rab7@queensu.ca>
2015-11-20 17:25:38 -0500

    Notification class implementation

--- src/Notification.cpp
+++ src/Notification.cpp
@@ -3,22 +3,20 @@
 using namespace std;
 
 Notification::Notification(const std::string& sender, NotificationTypeEnum noticeType, const Event& noticeEvent)
-{
-    //ctor
-}
+: from(sender), type(noticeType), event(noticeEvent) {}
 
 std::string Notification::getFrom()
 {
-
+    return from;
 }
 
 NotificationTypeEnum Notification::getType()
 {
-
+    return type;
 }
 
 Event Notification::getEvent()
 {
-
+    return event;
 }
 
