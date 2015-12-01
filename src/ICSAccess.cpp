9a924b202850aaeae123818ea1a8596809e84278
Ryan Baxter <12rab7@queensu.ca>
2015-11-30 23:51:39 -0500

    Bug fixes to ICSAccess

--- src/ICSAccess.cpp
+++ src/ICSAccess.cpp
@@ -68,6 +68,12 @@ vector<Event> ICSAccess::getEvents(){
 
 				} else if (Line.find("DTSTART") == 0) {
 					eventStart = getProperty(Line);
+
+					if (eventStart.length() < 15){
+                        useable = false;
+					}
+					else{
+
 					useable = true;
 
 					string date = eventStart.substr(0,8);
@@ -86,9 +92,15 @@ vector<Event> ICSAccess::getEvents(){
                     if (!WXstart.ParseTime(time.c_str())){
                         useable = false;
                     }
+					}
 
 				} else if (Line.find("DTEND") == 0) {
 					eventEnd = getProperty(Line);
+
+					if (eventEnd.length() < 15){
+                        useable = false;
+					}
+					else{
 
 					string date = eventEnd.substr(0,8);
 
@@ -107,6 +119,7 @@ vector<Event> ICSAccess::getEvents(){
                         useable = false;
                     }
 					hasEnd = true;
+					}
 
 				} else if (Line.find("SUMMARY") == 0) {
 					summary = getProperty(Line);
@@ -172,6 +185,10 @@ int ICSAccess::untilToCount(const string& firstTime, const string& freq, const s
     wxDateTime WXstart, WXend;
     wxTimeSpan WXdiff;
 
+    if (firstTime.length() < 15 || lastTime.length() < 15){
+        return -1;
+    }
+
     string date = firstTime.substr(0,8);
 
     date.insert(4,"-");
@@ -208,7 +225,7 @@ int ICSAccess::untilToCount(const string& firstTime, const string& freq, const s
     }
 
     if (WXstart.IsEarlierThan(WXend)){
-        WXdiff = WXstart.Subtract(WXend);
+        WXdiff = WXend.Subtract(WXstart);
         if (freq == "DAILY")
             return WXdiff.GetDays();
         else if (freq == "WEEKLY")
